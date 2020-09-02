33929bf25fa21b3cd4ad6661438201b0c3dbbb32
Grigor1994 <grigor.avetisyan@hotmail.com>
2020-09-02 15:17:23 +0400

    initial commit.

--- src/com/grigor/avetisyan/calculator/ArabicCalculator.java
+++ src/com/grigor/avetisyan/calculator/ArabicCalculator.java
@@ -3,25 +3,25 @@ package com.grigor.avetisyan.calculator;
 public class ArabicCalculator implements Calculator<Integer> {
     @Override
     public Integer plus(Integer a, Integer b) {
-        if (a > 10 || b > 10) throw new IllegalArgumentException(a + " or " + b + " must be equal or less than 10.");
+//        if (a > 10 || b > 10) throw new IllegalArgumentException(a + " or " + b + " must be equal or less than 10.");
         return a + b;
     }
 
     @Override
     public Integer minus(Integer a, Integer b) {
-        if (a > 10 || b > 10) throw new IllegalArgumentException(a + " or " + b + " must be equal or less than 10.");
+//        if (a > 10 || b > 10) throw new IllegalArgumentException(a + " or " + b + " must be equal or less than 10.");
         return a - b;
     }
 
     @Override
     public Integer multiply(Integer a, Integer b) {
-        if (a > 10 || b > 10) throw new IllegalArgumentException(a + " or " + b + " must be equal or less than 10.");
+//        if (a > 10 || b > 10) throw new IllegalArgumentException(a + " or " + b + " must be equal or less than 10.");
         return a * b;
     }
 
     @Override
     public Integer division(Integer a, Integer b) {
-        if (a > 10 || b > 10) throw new IllegalArgumentException(a + " or " + b + " must be equal or less than 10.");
+//        if (a > 10 || b > 10) throw new IllegalArgumentException(a + " or " + b + " must be equal or less than 10.");
         return a / b;
     }
 
--- src/com/grigor/avetisyan/calculator/Main.java
+++ src/com/grigor/avetisyan/calculator/Main.java
@@ -8,39 +8,54 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Input: ");
         String input = scanner.nextLine();
         String[] inputArray = input.split(" ");
+
+        String firstOperand = inputArray[0];
+        String secondOperand = inputArray[2];
+        String operator = inputArray[1];
+
         ArabicCalculator arabicCalculator = new ArabicCalculator();
         RomanCalculator romanCalculator = new RomanCalculator(arabicCalculator);
-        switch (inputArray[1]) {
+
+        boolean checker = false;
+        if (Parser.checkType(firstOperand, secondOperand)) {
+            checker =true;
+        }
+
+        switch (operator) {
             case "+" -> {
-                if (Parser.checkType(inputArray[0], inputArray[2])) {
-                    System.out.println("Output: \n" + romanCalculator.plus(inputArray[0], inputArray[2]));
+                if (checker) {
+                    System.out.println("Output: \n" + arabicCalculator.plus(Integer.parseInt(firstOperand), Integer.parseInt(secondOperand)));
                 } else {
-                    System.out.println("Output: \n" + arabicCalculator.plus(Integer.parseInt(inputArray[0]), Integer.parseInt(inputArray[2])));
+                    System.out.println("Output: \n" + romanCalculator.plus(firstOperand, secondOperand));
                 }
             }
             case "-" -> {
-                if (Parser.checkType(inputArray[0], inputArray[2])) {
-                    System.out.println("Output: \n" + romanCalculator.minus(inputArray[0], inputArray[2]));
+                if (checker) {
+                    System.out.println("Output: \n" + arabicCalculator.minus(Integer.parseInt(firstOperand), Integer.parseInt(secondOperand)));
                 } else {
-                    System.out.println("Output: \n" + arabicCalculator.minus(Integer.parseInt(inputArray[0]), Integer.parseInt(inputArray[2])));
+                    System.out.println("Output: \n" + romanCalculator.minus(firstOperand, secondOperand));
                 }
             }
             case "*" -> {
-                if (Parser.checkType(inputArray[0], inputArray[2])) {
-                    System.out.println("Output: \n" + romanCalculator.multiply(inputArray[0], inputArray[2]));
+                if (checker) {
+                    System.out.println("Output: \n" + arabicCalculator.multiply(Integer.parseInt(firstOperand), Integer.parseInt(secondOperand)));
                 } else {
-                    System.out.println("Output: \n" + arabicCalculator.multiply(Integer.parseInt(inputArray[0]), Integer.parseInt(inputArray[2])));
+                    System.out.println("Output: \n" + romanCalculator.multiply(firstOperand, secondOperand));
                 }
             }
             case "/" -> {
-                if (Parser.checkType(inputArray[0], inputArray[2])) {
-                    System.out.println("Output: \n" + romanCalculator.division(inputArray[0], inputArray[2]));
-                } else {
+                if (checker) {
                     System.out.println("Output: \n" + arabicCalculator.division(Integer.parseInt(inputArray[0]), Integer.parseInt(inputArray[2])));
+                } else {
+                    System.out.println("Output: \n" + romanCalculator.division(firstOperand, secondOperand));
                 }
+            }
+            default -> {
+                throw new IllegalArgumentException("`" + operator + "`" + " is not mathematical operator.");
             }
         }
     }
